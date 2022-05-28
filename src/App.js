8a0f871582a9b1cd2e39622985743b3469e57752
Pawar-Darshan <officialdarshan007@gmail.com>
2022-05-28 18:54:58 +0530

    ludogame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      <LudoGame />
     </div>
   );
 }
+
+const ludogame1 = () => {
+  const play1 = Math.floor(Math.random() * 6) + 1;
+  const img = `/assets/Images/Dice_${play1}.png`;
+  return img;
+};
+
+const ludogame2 = () => {
+  const play2 = Math.floor(Math.random() * 6) + 1;
+  const img = `/assets/Images/Dice_${play2}.png`;
+  return img;
+};
+
+function refreshPage() {
+  window.location.reload(false);
+}
+
+const LudoGame = () => {
+  return (
+    <div className="container">
+      <h1>Ludo Game</h1>
+      <div className="dice">
+        <p>Player 1</p>
+        <img src={ludogame1()} alt={ludogame1()} />
+        {console.log()}
+      </div>
+      <div className="dice">
+        <p>Player 2</p>
+        <img src={ludogame2()} alt={ludogame2()} />
+      </div>
+      <div>
+        <button onClick={refreshPage}>Click Me</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/images.js
@@ -0,0 +1,20 @@
+export const images = [
+  {
+    image: "./Images/Dice_1.png",
+  },
+  {
+    image: "./Images/Dice_2.png",
+  },
+  {
+    image: "./Images/Dice_3.png",
+  },
+  {
+    image: "./Images/Dice_4.png",
+  },
+  {
+    image: "./Images/Dice_5.png",
+  },
+  {
+    image: "./Images/Dice_6.png",
+  },
+];
